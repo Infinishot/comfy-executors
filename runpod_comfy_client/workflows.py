81da1176ca645cbdd9d3bbedb3f937f23d940e45
Kristian Klemon <kristian.klemon@gmail.com>
2024-04-09 09:47:36 +0000

    Improve interface of WorkflowTemplate

--- runpod_comfy_client/workflows.py
+++ runpod_comfy_client/workflows.py
@@ -1,14 +1,14 @@
 import json
+import requests
 
-from pathlib import Path
 from jinja2 import Template, StrictUndefined, meta
 
 
 class WorkflowTemplate:
     REQUIRED_VARIABLES = ["input_images_dir", "batch_size"]
 
-    def __init__(self, filename):
-        self.workflow_template_raw = Path(filename).read_text()
+    def __init__(self, template_string: str):
+        self.workflow_template_string = template_string
         self.workflow_template = Template(
             self.workflow_template_raw, undefined=StrictUndefined
         )
@@ -20,6 +20,17 @@ class WorkflowTemplate:
             raise ValueError(
                 f"Missing variables in workflow template: {set(self.REQUIRED_VARIABLES) - undeclared_variables}"
             )
+
+    @classmethod
+    def from_file(cls, filename):
+        with open(filename) as f:
+            return cls(f.read())
+
+    @classmethod
+    def from_url(cls, url):
+        response = requests.get(url)
+        response.raise_for_status()
+        return cls(response.text)
 
     def render(self, **kwargs):
         return json.loads(self.workflow_template.render(**kwargs))
